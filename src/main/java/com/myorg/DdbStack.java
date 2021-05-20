7e6295517e6e92bb59e9d8a85a0034734816281f
guirodg <guizinhord@hotmail.com>
2021-05-20 15:39:37 -0300

    feat: adiciona auto scale do db dynamo

--- src/main/java/com/myorg/DdbStack.java
+++ src/main/java/com/myorg/DdbStack.java
@@ -1,16 +1,11 @@
 package com.myorg;
 
-import software.amazon.awscdk.core.Construct;
-import software.amazon.awscdk.core.RemovalPolicy;
-import software.amazon.awscdk.core.Stack;
-import software.amazon.awscdk.core.StackProps;
-import software.amazon.awscdk.services.dynamodb.Attribute;
-import software.amazon.awscdk.services.dynamodb.AttributeType;
-import software.amazon.awscdk.services.dynamodb.BillingMode;
-import software.amazon.awscdk.services.dynamodb.Table;
+import software.amazon.awscdk.core.*;
+import software.amazon.awscdk.services.dynamodb.*;
 
 public class DdbStack extends Stack {
     private final Table productEventsDbd;
+
     public DdbStack(final Construct scope, final String id) {
         this(scope, id, null);
     }
@@ -34,6 +29,27 @@ public class DdbStack extends Stack {
                 .timeToLiveAttribute("ttl")
                 .removalPolicy(RemovalPolicy.DESTROY) // Caso apagar a stack destruir a tabela
                 .build();
+
+        // Auto Scale DB Leitura
+        productEventsDbd.autoScaleReadCapacity(EnableScalingProps.builder()
+                .minCapacity(1)
+                .maxCapacity(4)
+                .build())
+                .scaleOnUtilization(UtilizationScalingProps.builder()
+                        .targetUtilizationPercent(50)
+                        .scaleInCooldown(Duration.seconds(30))
+                        .scaleOutCooldown(Duration.seconds(30))
+                        .build());
+        // Auto Scale DB Escrita
+        productEventsDbd.autoScaleWriteCapacity(EnableScalingProps.builder()
+                .minCapacity(1)
+                .maxCapacity(4)
+                .build())
+                .scaleOnUtilization(UtilizationScalingProps.builder()
+                        .targetUtilizationPercent(50)
+                        .scaleInCooldown(Duration.seconds(30))
+                        .scaleOutCooldown(Duration.seconds(30))
+                        .build());
     }
 
     public Table getProductEventsDbd() {
